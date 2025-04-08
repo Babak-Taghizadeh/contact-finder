18f38a6cfdaee2e8c0f4d9db6ee186f9e2db5b62
Babak Taghizadeh <babak.tz98@gmail.com>
2025-04-08 17:20:10 +0330

    feat: Add react-router setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import { createRoot } from "react-dom/client";
 import "./styles/main.scss";
 import App from "./App";
 import { StrictMode } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
