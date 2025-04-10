26cb59801c6cee8c6b2c4d7b439cc8c3bc857808
Babak Taghizadeh <babak.tz98@gmail.com>
2025-04-10 21:25:30 +0330

    feat: Add infinite scroll feature to contacts list for better performance

--- src/components/pages/contact-list/contacts-list.tsx
+++ src/components/pages/contact-list/contacts-list.tsx
@@ -1,25 +1,49 @@
+import { useInfiniteContacts } from "@/hooks/use-infinite-contacts";
 import ContactCard from "./contact-card";
+import { useCallback, useRef } from "react";
 
 const ContactsList = () => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteContacts();
+
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastElementRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (isFetchingNextPage) return;
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [fetchNextPage, hasNextPage, isFetchingNextPage]
+  );
+
+  const contacts = data?.pages.flatMap((page) => page.items) || [];
+  if (status === "pending") {
+    return <div>Loading initial data...</div>;
+  }
+
+  if (status === "error") {
+    return <div>Error loading contacts</div>;
+  }
   return (
     <div className="contacts-container">
-      <ContactCard
-        contact={{
-          address: "test",
-          avatar: null,
-          company: "test",
-          createdAt: 1,
-          email: "test",
-          first_name: "test",
-          gender: "test",
-          id: 1,
-          last_name: "test",
-          note: "test",
-          phone: "test",
-          telegram: "test",
-          updatedAt: 1,
-        }}
-      />
+      {contacts.map((contact, index) => {
+        return (
+          <div
+            ref={index === contacts.length - 1 ? lastElementRef : null}
+            key={`${contact.id}-${index}`}
+          >
+            <ContactCard contact={contact} />
+          </div>
+        );
+      })}
+      {isFetchingNextPage && <div>Loading more contacts...</div>}
     </div>
   );
 };
