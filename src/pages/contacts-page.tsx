79e7498222dec640ee75c80d13db44a7647ba6c4
Babak Taghizadeh <babak.tz98@gmail.com>
2025-04-11 04:45:58 +0330

    feat: Add recent contacts components and styles

--- src/pages/contacts-page.tsx
+++ src/pages/contacts-page.tsx
@@ -1,8 +1,10 @@
 import ContactsList from "@/components/pages/contact-list/contacts-list";
+import { RecentContacts } from "@/components/pages/contact-list/recent-contacts";
 
 const ContactPage = () => {
   return (
     <>
+      <RecentContacts />
       <ContactsList />
     </>
   );
